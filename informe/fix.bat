36aea34b736ba983dd6fc2e93c158d73285d988d
Xabierland <xabierland@gmail.com>
2023-09-27 19:13:51 +0200

    DOC

---
+++ informe/fix.bat
@@ -0,0 +1,11 @@
+taskkill /F /IM pdflatex.exe
+
+del informe.aux
+del informe.fls
+del informe.log
+del informe.synctex(busy)
+del informe.synctex.gz
+del informe.toc
+del informe.fdb_latexmk
+
+REM pause
